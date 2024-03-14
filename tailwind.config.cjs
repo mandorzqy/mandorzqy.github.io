c3ea17032c5e12ba4a0fd3dfd897e0de32337a66
MUHAMMAD ARMANDO NUR RIZQY ANSAR <armandorizqy2326@gmail.com>
2024-03-15 00:04:28 +0700

    Update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Poppins", "system-ui"],
+		}},
 	},
 	plugins: [],
 }
