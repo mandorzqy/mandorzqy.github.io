bc9093c28889c0d2ea7baaa233665075555b1520
MUHAMMAD ARMANDO NUR RIZQY ANSAR <armandorizqy2326@gmail.com>
2024-05-30 05:15:53 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mandorzqy.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react()]
 });
